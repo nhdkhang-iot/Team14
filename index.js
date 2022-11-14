269c73caa6fb25aaf15e1757ad967783c4242872
Nguyễn Huệ Duy Khang <116970766+nhdkhang-iot@users.noreply.github.com>
2022-11-14 08:08:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  //Điền thông tin Firebase config của cá nhân vào đây
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9OeeauxISrlEmkCo8T8U3TB0foaFRqtk",
+  authDomain: "kc326-f1fbd.firebaseapp.com",
+  databaseURL: "https://kc326-f1fbd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kc326-f1fbd",
+  storageBucket: "kc326-f1fbd.appspot.com",
+  messagingSenderId: "720706589594",
+  appId: "1:720706589594:web:63ee8d785f813a70722023"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+};
+
+
